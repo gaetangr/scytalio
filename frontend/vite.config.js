201a292f8bb00b20739c16be1d7bc4aa7355005e
Gaëtan <hello@gaetangrond.me>
2025-01-03 21:02:06 +0100

    Remove VueJS devtool

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
@@ -12,7 +10,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    vueDevTools(),
+  
   ],
   resolve: {
     alias: {
