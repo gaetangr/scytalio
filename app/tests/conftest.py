50322ec61cd7b9efbc28809a63137000659d7d34
Gaëtan <hello@gaetangrond.me>
2025-01-04 19:52:21 +0100

    Add conftest for better testing and fix failing test

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,9 @@
 from fastapi.testclient import TestClient
 import pytest
 from sqlmodel import Session
-from sqlalchemy.orm import sessionmaker
 
 
-from app.database import create_db_and_tables, engine
+from app.database import engine
 import pytest
 from app.main import app
 
@@ -13,12 +12,9 @@ from app.main import app
 # or resources needed for tests and reuse them in different tests.
 # https://docs.pytest.org/en/stable/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 @pytest.fixture(scope="module")
 def session():
-    create_db_and_tables()
     with Session(engine) as session:
         yield session
 
--- app/tests/test_endpoints.py
+++
@@ -1,8 +0,0 @@
-def test_encrypt_endpoint(client):
-    response = client.post("/encrypt", json={"message": "test", "iv": "test"})
-    assert response.status_code == 201
-
-
-def test_decrypt_endpoint_with_non_uuid(client):
-    response = client.get("/decrypt/1/")
-    assert response.status_code == 400
